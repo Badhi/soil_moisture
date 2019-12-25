1db8c41b0b25c4afe0085b0d3f54a2310037c8f9
Bhashitha Adhikarinayake <bmadhikarinayake@gmail.com>
2019-12-25 20:03:07 +0530

    Complete code for on sensor with fan and pump

--- sketch_dec15a.ino
+++ sketch_dec15a.ino
@@ -1,24 +1,73 @@
+#include <EEPROM.h>
+
 #define HOLD_COUNT 100
 
+//#define COM
+
+#define EEPROM_SOIL_MOISTURE_LOW_ADDR 0
+#define EEPROM_SOIL_MOISTURE_HIGH_ADDR 1
+//#define TEST
+
+#ifdef COM
 #define SEND(message) \
 {\
   Serial.println(message); \
 }
-const int motor0 = 7;
+#else
+#define SEND(message)
+#endif 
+
+#define FSEND(...)\
+{\
+  sprintf(printBuff, __VA_ARGS__);\
+  SEND(printBuff)\
+}
+const int motor0 = 8;
+const int fan0 = 9;
+bool motor0High = false;
+bool fan0High = false;
+
 const int pushButton = 13;
 
 int soilMoistureA0 = 0;
 char printBuff[100] ;
 
-int soilMoistureLowVal = 0;
-int soilMoistureHighVal = 550;
+int soilMoistureLowVal = 341;
+int soilMoistureHighVal = 587;
 
-float soilMoisture0_threshold = 0.5;
+int soilMoisture0_pumpThreshold = 60;
+int soilMoisture0_fanThreshold = 40;
 
-inline bool thresholdReached(float value){
-  return ((value - soilMoistureLowVal)/(soilMoistureHighVal - soilMoistureLowVal)) > soilMoisture0_threshold;
+#ifndef TEST
+inline bool waterThresholdReached(float value){
+  int ratio = 100 * ((value - soilMoistureLowVal)/(soilMoistureHighVal - soilMoistureLowVal));
+  FSEND("Water : Ratio : %d, thresh : %d", (int) ratio, (int) (soilMoisture0_pumpThreshold));
+  return ratio > soilMoisture0_pumpThreshold ;
+}
+#else
+inline bool waterThresholdReached(float value){
+  return (value > 550) ? true : false; 
+}
+#endif
+
+inline bool fanThresholdReached(float value){
+  int ratio = 100 * ((value - soilMoistureLowVal)/(soilMoistureHighVal - soilMoistureLowVal));
+  FSEND("Fan : Ratio : %d, thresh : %d", (int) ratio, (int) (soilMoisture0_fanThreshold));
+  return ratio < soilMoisture0_fanThreshold ;
 }
 
+inline int readEEPROM(int address32){
+  int value = 0;
+  for(int i = 0; i < 4; i++){
+    value |= (EEPROM.read(address32 * 4 + i)) << (i * 8);
+  }
+  return value;
+}
+inline void writeEEPROM(int address32, int value){
+  for(int i= 0; i < 4; i++){
+    EEPROM.write(address32 * 4 + i, (value >> i * 8) & 0xff);
+  }
+}
 bool checkPressed(const int pin)
 {
   long pressedCount = 0;
@@ -56,18 +105,18 @@ void calibrateMoistureSensor()
 {
   delay(2000);
   SEND("Calibrating Moisture Sensor");
-  SEND("Put the sensor to the lowest moisture and press button");
- 
-  soilMoistureLowVal = getValueOnPressButton(pushButton, A0);
-  sprintf(printBuff, "Calibration done, Low = %d", soilMoistureLowVal);
-  SEND(printBuff);
 
   SEND("Put the sensor to the highest moisture level and press button");
-
   soilMoistureHighVal = getValueOnPressButton(pushButton, A0);
+  FSEND("Calibration done, High = %d", soilMoistureHighVal);
+  
+  SEND("Put the sensor to the lowest moisture and press button");
+  soilMoistureLowVal = getValueOnPressButton(pushButton, A0);
+  FSEND("Calibration done, Low = %d", soilMoistureLowVal);
 
-  sprintf(printBuff, "Calibration done, High = %d", soilMoistureHighVal);
-  SEND(printBuff);
+  SEND("Updating the ROM");
+  writeEEPROM(EEPROM_SOIL_MOISTURE_LOW_ADDR, soilMoistureLowVal);
+  writeEEPROM(EEPROM_SOIL_MOISTURE_HIGH_ADDR, soilMoistureHighVal);
 
   SEND("Press button to proceed");
   while(!checkPressed(pushButton));
@@ -75,36 +124,78 @@ void calibrateMoistureSensor()
 
 void setup()
 {
+
+#ifdef COM
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
     ; // wait for serial port to connect. Needed for Native USB only
   }
+#endif
+ 
   pinMode(motor0, OUTPUT);
+  pinMode(fan0, OUTPUT);
+  digitalWrite(motor0, LOW);
+  digitalWrite(fan0, LOW);
+  
   pinMode(pushButton, INPUT);
+
+  soilMoistureLowVal = readEEPROM(EEPROM_SOIL_MOISTURE_LOW_ADDR);
+  soilMoistureHighVal = readEEPROM(EEPROM_SOIL_MOISTURE_HIGH_ADDR);
+
+  FSEND("EEPROM Read Moisture High : %d, Moisture Low : %d", soilMoistureLowVal, soilMoistureHighVal);
   SEND("Setup completed");
+}
+
+void checkForPump(int sensorValue){
+  if(waterThresholdReached(sensorValue)){
+    if(!motor0High){
+      motor0High = true;
+      SEND("Setting the Motor 0 to high");
+      digitalWrite(motor0, HIGH);
+    }
+  }
+  else{
+    if(motor0High){
+      motor0High = false;
+      SEND("Setting the Motor 0 to low");
+      digitalWrite(motor0, LOW);
+    }
+  }  
+}
+
+void checkForFan(int sensorValue){
+  if(fanThresholdReached(sensorValue)){
+    if(!motor0High){
+      fan0High = true;
+      SEND("Setting the Fan 0 to high");
+      digitalWrite(fan0, HIGH);
+    }
+  }
+  else{
+    if(motor0High){
+      fan0High = false;
+      SEND("Setting the Fan 0 to low");
+      digitalWrite(fan0, LOW);
+    }
+  }    
 }
 
 void loop() // run over and over
 {
+#ifdef COM
   if(checkPressed(pushButton)){
     SEND("Calibrating");
     calibrateMoistureSensor();
   }
+#endif
     
   soilMoistureA0 = analogRead(A0);
 
-  sprintf(printBuff, "Moisture Read Value on A0 : %d", soilMoistureA0);  
-  SEND(printBuff);
-
-  if(thresholdReached(soilMoistureA0)){
-    SEND("Setting the Motor 0 to high");
-    digitalWrite(motor0, HIGH);
-  }
-  else{
-    SEND("Setting the Motor 0 to low");
-    digitalWrite(motor0, LOW);
-  }
+  FSEND("Moisture Read Value on A0 : %d", soilMoistureA0);
   
-   delay(100);
+  checkForPump(soilMoistureA0);
+  checkForFan(soilMoistureA0);
+  
+  delay(100);
 }
